4fff0f3cc9192817066caac9e7ce6225af6ee51d
Jeremie Jost <jeremiejost@gmail.com>
2017-11-03 20:05:20 +0000

    ... working!

--- Board.elm
+++ Board.elm
@@ -8,6 +8,9 @@ import Random
 import Cell
 import Window
 import Task
+import Time exposing (Time, second)
+import Debug
+import Set
 
 main =
     Html.program
@@ -39,22 +42,30 @@ init : ( Model, Cmd Msg )
 init =
     let
         gameSurface = { vsize = 0, hsize = 0 }
-        model = { aliveCells = [(4,4), (4, 5), (5, 5), (6, 6), (7, 7), (8, 10), (20,2), (22,3), (22,15)], gameSurface = gameSurface }
+        model = { aliveCells = [(4,4), (4, 5), (4, 6), (8, 10), (20,2), (22,3), (22,15)], gameSurface = gameSurface }
     in
         (model, Task.perform ResizeBoard Window.size)
 
 
--- FIXME
 neighbors : Coords -> List Coords
 neighbors ( i, j ) =
-    [ ( i, j ), ( i - 1, j ), ( i + 1, j ), ( i, j - 1 ), ( i, j + 1 ) ]
+    [ ( i - 1, j - 1 )
+    , ( i - 1, j )
+    , ( i - 1, j + 1 )
+    , ( i, j - 1 )
+    , ( i, j + 1 )
+    , ( i + 1, j - 1 )
+    , ( i + 1, j )
+    , ( i + 1, j + 1 )
+    ]
 
+aliveNeighbors : Model -> Coords -> Int
+aliveNeighbors { aliveCells, gameSurface } coords =
+    neighbors coords |> List.filter (\c -> List.member c aliveCells) |> List.length
 
--- might be needed prob not
-indexedMap : (Coords -> a -> b) -> List (List a) -> List (List b)
-indexedMap f board =
-    board
-        |> List.indexedMap (\i row -> row |> List.indexedMap (\j cellModel -> f ( i, j ) cellModel))
+zombieCells : Model -> List Coords
+zombieCells { aliveCells, gameSurface } =
+        aliveCells |> List.concatMap neighbors |> Set.fromList |> Set.toList
 
 
 cellSize = 50
@@ -62,13 +73,30 @@ cellSize = 50
 
 -- Update
 
-type Msg = ResizeBoard Window.Size
+type Msg
+    = ResizeBoard Window.Size
+    | Tick Time
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
         ResizeBoard { width, height } ->
             { model | gameSurface = { hsize = width // cellSize, vsize = height // cellSize } } ! []
+        Tick _ ->
+            let
+                stillAliveCells = model.aliveCells |> List.filter (cellSurvives model)
+                newbornCells = zombieCells model |> List.filter (\cell -> aliveNeighbors model cell == 3)
+            in
+               { model | aliveCells = stillAliveCells ++ newbornCells } ! []
+
+cellSurvives : Model -> Coords -> Bool
+cellSurvives model cell =
+    let
+       aliveNeighborsCount = aliveNeighbors model cell
+    in
+       List.member aliveNeighborsCount [2, 3]
+
+
 
 
 -- SUBSCRIPTIONS
@@ -76,7 +104,10 @@ update message model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Window.resizes ResizeBoard
+    Sub.batch
+        [ Window.resizes ResizeBoard
+        , Time.every second Tick
+        ]
 
 -- VIEW
 
